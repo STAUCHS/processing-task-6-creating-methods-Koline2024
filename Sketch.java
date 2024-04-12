5b1ea1cc63d7c1a4998f2d2fce991dba3fe739d7
koline2024 <christopherxia2020@gmail.com>
2024-04-12 13:39:24 -0400

    a

--- Sketch.java
+++ Sketch.java
@@ -24,7 +24,7 @@ public class Sketch extends PApplet {
     // For loop creates multiple flowers with drawFlower function
     for(int intFlowerX = 50; intFlowerX < width; intFlowerX = intFlowerX + 100){
       for(int intFlowerY = 50; intFlowerY < height; intFlowerY = intFlowerY + 100){
-        drawFlower(intFlowerX, intFlowerY, 55, 200, 0);
+        drawFlower(intFlowerX, intFlowerY, 100, 0, 90);
         
       }
     } 
